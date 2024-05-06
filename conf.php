cd416a439b16d7d52f0e2e422561992fe9a3fe08
OleksandrB <alexboggg361@gmail.com>
2024-05-06 10:28:57 +0300

    muudatud conf fail

--- conf.php
+++ conf.php
@@ -1,8 +1,8 @@
 <?php
-$kasutaja='d123178_arkadiko';
-$serverinimi='d123178.mysql.zonevs.eu';
-$parool='markiz2020!';
-$andmebaas='d123178_akbaas';
+$kasutaja='oleksandr';
+$serverinimi='localhost';
+$parool='123';
+$andmebaas='bohatyrov';
 $yhendus=new mysqli($serverinimi,$kasutaja,$parool,$andmebaas);
 $yhendus->set_charset('UTF8');
 
